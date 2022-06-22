db7408169bd028d5ed742f2516cab029346ddffd
Jose Estrada <joseestrada@Apples-MacBook-Pro.local>
2022-06-21 18:04:43 -0600

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: 'YOUR_KEY_HERE_AIzaSyAOWH',
+  authDomain: 'your-auth-domain-b1234.firebaseapp.com',
+  databaseURL: 'https://your-database-name.firebaseio.com',
+  projectId: 'your-project-id-1234',
+  storageBucket: 'your-project-id-1234.appspot.com',
+  messagingSenderId: '12345-insert-yourse',
+  appId: 'insert yours: 1:1234:web:ee873bd1234c0deb7eba61ce',
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  firebase
+    .firestore()
+    .settings({merge: true, experimentalForceLongPolling: true}); //add this..
+}
+
+export {firebase};
